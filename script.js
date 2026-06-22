5e20275422ed85d18a4f349765e1c60e2b0646a7
Ane♡ <almeida.anelize@escola.pr.gov.br>
2026-06-22 12:04:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ function insert(num) {
 function clean(){
 document.getElementById('resultado').innerHTML="";
 }
-function deack(){
-     var numero=document.getElementById('resultado').innerHTML;
+function back(){
+     var reultado=document.getElementById('resultado').innerHTML;
      document.getElementById('resultado').innerHTML=resultado.substeing(0,resultado.length-1);
     }
+function calcular(){
+    var resultado= document.getElementById('resultado').innerHTML;
+if(reultado){
+    document.getElementById('resultado').innerHTML=eval(resultado);
+}
+else{
+    document.getElementById('resultado').innerHTML="nada para calcular"
+}
+}
